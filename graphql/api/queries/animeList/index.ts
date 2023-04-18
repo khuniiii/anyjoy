380e5340446f008f04a9afc0630810635f7d1b5a
Kyeonghun <48789893+khuniiii@users.noreply.github.com>
2023-04-19 01:55:31 +0900

    feat: gql로 db정보 불러오기 (실패)

---
+++ graphql/api/queries/animeList/index.ts
@@ -0,0 +1,25 @@
+import { Anime } from "@/lib/model/anime";
+import AnimeList from "./typeDef.graphql";
+
+export const getAnimeList = async (
+  _: unknown,
+  { input }: { input: { id: number } },
+): Promise<typeof AnimeList> => {
+  const anime = await Anime.findOne({ id: input.id });
+
+  if (!anime) {
+    throw new Error(`Anime with ID ${input.id} not found`);
+  }
+
+  return {
+    title: anime.title,
+    author: anime.author,
+    company: anime.company,
+    is_adult: anime.is_adult,
+    genre: anime.genre,
+    year: anime.year,
+    branch: anime.branch,
+    is_ended: anime.is_ended,
+    id: anime.id,
+  };
+};
