eb7cb03b3ae87b658e331c4cb8ccec9c5c1327c2
doby-onuii <rudgns517@naver.com>
2023-04-25 14:14:30 +0900

    fix: Query를 하나로 줄임

--- graphql/api/queries/animeList/index.ts
+++ graphql/api/queries/animeList/index.ts
@@ -2,29 +2,16 @@ import { Anime } from "@/lib/model/anime";
 
 export const getAnimeList = async (
   _: unknown,
-  { input }: { input: { is_show: boolean } },
+  { input }: { input: { is_show: boolean; title: string } },
 ): Promise<unknown> => {
   const anime = await Anime.find({
-    is_show: input.is_show,
+    $or: [{ is_show: input.is_show }, { title: input.title }],
   });
 
   if (anime.length === 0) {
-    throw new Error(`Anime with is_show ${input.is_show} not found`);
-  }
-
-  return anime;
-};
-
-export const getAnimeByTitle = async (
-  _: unknown,
-  { input }: { input: { title: string } },
-): Promise<unknown> => {
-  const anime = await Anime.find({
-    title: input.title,
-  });
-
-  if (anime.length === 0) {
-    throw new Error(`Anime with title ${input.title} not found`);
+    throw new Error(
+      `Anime with is_show ${input.is_show} not found or title ${input.title} not found`,
+    );
   }
 
   return anime;
