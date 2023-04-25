958f6cf4a387c2497160b6728e952bd7b7da8e8c
doby-onuii <rudgns517@naver.com>
2023-04-25 10:47:27 +0900

    feat: getAnimeList에 input으로 title 추가 및 리스트 뷰 수정

--- components/organisms/MainList/style.ts
+++ components/organisms/MainList/style.ts
@@ -2,12 +2,19 @@ import { styled } from "stitches.config";
 
 export const ContentContainer = styled("div", {
   width: "100%",
-  display: "flex",
-  flexWrap: "wrap",
-  justifyContent: "center",
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fit, minmax(350px, 1fr))",
+  gridGap: "10px",
 });
 
 export const Content = styled("div", {
-  flex: "0 1 456px",
-  margin: "10px",
+  display: "grid",
+  placeItems: "center",
+  marginTop: "30px",
+});
+
+export const Title = styled("p", {
+  fontWeight: "500",
+  fontSize: "20px",
+  paddingTop: "10px",
 });
