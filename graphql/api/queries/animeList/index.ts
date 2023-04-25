db45ac7081d0a60fe6967e0f0752adda2cdb36b5
doby-onuii <rudgns517@naver.com>
2023-04-25 12:46:05 +0900

    feat: title 제거

--- graphql/api/queries/animeList/index.ts
+++ graphql/api/queries/animeList/index.ts
@@ -2,17 +2,14 @@ import { Anime } from "@/lib/model/anime";
 
 export const getAnimeList = async (
   _: unknown,
-  { input }: { input: { is_show: boolean; title: string } },
+  { input }: { input: { is_show: boolean } },
 ): Promise<unknown> => {
   const anime = await Anime.find({
     is_show: input.is_show,
-    title: input.title,
   });
 
   if (anime.length === 0) {
-    throw new Error(
-      `Anime with is_show ${input.is_show} not found or title ${input.title} not found`,
-    );
+    throw new Error(`Anime with is_show ${input.is_show} not found`);
   }
 
   return anime;
