61beb068c385766be4817430259b096ee086d72b
doby-onuii <rudgns517@naver.com>
2023-04-27 20:02:25 +0900

    feat: 회원가입 폼 제작

--- components/organisms/JoinUser/index.tsx
+++ components/organisms/JoinUser/index.tsx
@@ -1,20 +1,42 @@
 import { signIn, useSession, signOut } from "next-auth/react";
+import {
+  Container,
+  SocialBtn,
+  SocialGroup,
+  JoinGroup,
+  JoinInput,
+  JoinBtn,
+} from "./style";
 
 const JoinUser = () => {
-  const { data: session } = useSession();
-  console.log(session);
+  const { data, status } = useSession();
+  console.log(data, status);
   return (
     <>
-      {session ? (
+      {data ? (
         <>
-          {session.user?.name}님 반갑습니다
+          {data.user?.name}님 반갑습니다
           <button onClick={() => signOut()}>로그아웃</button>
         </>
       ) : (
-        <>
-          <button onClick={() => signIn("naver")}>네이버</button>
-          <button onClick={() => signIn("kakao")}>카카오</button>
-        </>
+        <Container>
+          <JoinGroup>
+            <JoinInput placeholder="이메일"></JoinInput>
+            <JoinInput placeholder="이름"></JoinInput>
+            <JoinInput placeholder="비밀번호"></JoinInput>
+            <JoinInput placeholder="전화번호"></JoinInput>
+            <JoinInput placeholder="생년월일"></JoinInput>
+            <JoinBtn>회원가입하기</JoinBtn>
+          </JoinGroup>
+          <SocialGroup>
+            <SocialBtn social="naver" onClick={() => signIn("naver")}>
+              네이버로 로그인
+            </SocialBtn>
+            <SocialBtn social="kakao" onClick={() => signIn("kakao")}>
+              카카오로 로그인
+            </SocialBtn>
+          </SocialGroup>
+        </Container>
       )}
     </>
   );
