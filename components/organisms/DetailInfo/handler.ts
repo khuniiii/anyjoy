956bc29b895ca5b96f98d927dfe281beb18b172b
doby-onuii <48789893+khuniiii@users.noreply.github.com>
2023-08-09 17:59:56 +0900

    feat: detail info에서 id로 한개의 정보만 조회

---
+++ components/organisms/DetailInfo/handler.ts
@@ -0,0 +1,26 @@
+import { StatesType } from "./type";
+
+const useHandlers = (states: StatesType) => {
+  const { getAnimeById, setAniInfo } = states;
+
+  const getAnimeByIdData = async (id: number) => {
+    console.log("id:", id, typeof id);
+    try {
+      const { data } = await getAnimeById({
+        variables: {
+          input: {
+            is_show: true,
+            id: id,
+          },
+        },
+      });
+      setAniInfo(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { getAnimeByIdData };
+};
+
+export default useHandlers;
