03f5df26504406860b3e7989a1d9a60f64208237
khnuiiii <48789893+khuniiii@users.noreply.github.com>
2023-09-11 23:36:31 +0900

    feat: 조회수, 날짜순 정렬 기능 추가

--- components/organisms/PostList/index.tsx
+++ components/organisms/PostList/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import useStates from "@/components/organisms/PostList/state";
 import useHandlers from "@/components/organisms/PostList/handler";
@@ -23,28 +23,61 @@ const PostList = ({ title }: { title: string }) => {
     getPostByTypeData(title);
   }, []);
 
+  const tempArr = postList?.getPostByType;
+
+  const viewOrderArray =
+    tempArr &&
+    [...tempArr].sort((item1, item2) => {
+      const views1 = item1.views;
+      const views2 = item2.views;
+      return views2 - views1;
+    });
+
+  const timeOrderArray =
+    tempArr &&
+    [...tempArr].sort((item1, item2) => {
+      const date1 = item1.createdAt;
+      const date2 = item2.createdAt;
+      return date2 - date1;
+    });
+
+  const [arrayType, setArrayType] = useState(true);
+  const [arr, setArr] = useState<Array>(timeOrderArray);
+
+  useEffect(() => {
+    setArr(timeOrderArray);
+    if (arrayType) {
+      setArr(viewOrderArray);
+    } else {
+      setArr(timeOrderArray);
+    }
+  }, [arrayType]);
+
   return (
     <>
       <div style={{ padding: "10px" }}>
         <PostListContainer>
           <Title size="main">{title} 게시판</Title>
-          {postList?.getPostByType.map((item, index) => {
-            return (
-              <PostWrapper
-                key={`${item.type}-${index}`}
-                onClick={() => movePostId(item._id)}
-              >
-                <Title size="item">{item.title}</Title>
-                <PostInfo>
-                  <p>조회수: {item.views}&nbsp;</p>
-                  <p>
-                    생성일자:{" "}
-                    {item.createdAt ? formattedDate(item.createdAt) : ""}
-                  </p>
-                </PostInfo>
-              </PostWrapper>
-            );
-          })}
+          <button onClick={() => setArrayType(false)}>날짜순</button>
+          <button onClick={() => setArrayType(true)}>조회순</button>
+          {arr &&
+            arr.map((item, index) => {
+              return (
+                <PostWrapper
+                  key={`${item.type}-${index}`}
+                  onClick={() => movePostId(item._id)}
+                >
+                  <Title size="item">{item.title}</Title>
+                  <PostInfo>
+                    <p>조회수: {item.views}&nbsp;</p>
+                    <p>
+                      생성일자:{" "}
+                      {item.createdAt ? formattedDate(item.createdAt) : ""}
+                    </p>
+                  </PostInfo>
+                </PostWrapper>
+              );
+            })}
 
           <CreateBtn onClick={() => movePostCreate(title)}>
             작성하러 가기
